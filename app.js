56d7162a5fe7c4a1007c5015198d3448cbee77a9
chorsey <caylahorsey@gmail.com>
2020-02-08 15:39:59 -0500

    BASE ADDED

---
+++ app.js
@@ -0,0 +1,22 @@
+const divContainer = document.getElementById("container")
+//const div1 = document.createElement('div');
+
+//divContainer.appendChild(div1);
+
+
+//Create divs
+for (i = 0; i < 32; i++) {
+    const div = document.createElement('div')
+    divContainer.appendChild(div)
+    console.log('so many divs')
+};
+
+
+
+
+
+//const h1 = document.createElement('h1')
+//const h1Text = document.createTextNode('header text')
+//h1.appendChild(h1Text);
+//div1.appendChild(h1);
+//h1.style.color = 'blue'
